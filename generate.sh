a99d384dbabf5450b459ee1762fe4d96de91638b
pawelwlazlo <pawel@wlazlo.dev>
2023-10-23 09:16:41 +0200

    adding package name to options

--- generate.sh
+++ generate.sh
@@ -5,6 +5,7 @@ MAIN_PACKAGE="$1"
 docker run --rm -v "${PWD}":/local openapitools/openapi-generator-cli generate \
   --library jvm-spring-webclient  -i /local/woocommerce-openapi-3.0.x.yml -g kotlin \
    -o /local/out/woocommerce-client \
+    --package-name "${MAIN_PACKAGE}.woocommerce" \
     --model-package "${MAIN_PACKAGE}.woocommerce.model" \
     --invoker-package "${MAIN_PACKAGE}.data.woocommerce" \
     --api-package "${MAIN_PACKAGE}.woocommerce.api" \
